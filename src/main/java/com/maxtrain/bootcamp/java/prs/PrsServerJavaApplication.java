9c92ecc748ea2ed0574bab317e076880d8e4b2fa
gpdoud <gpdoud@gmail.com>
2023-03-24 14:51:23 -0400

    initial commit

---
+++ src/main/java/com/maxtrain/bootcamp/java/prs/PrsServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.maxtrain.bootcamp.java.prs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsServerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrsServerJavaApplication.class, args);
+	}
+
+}
